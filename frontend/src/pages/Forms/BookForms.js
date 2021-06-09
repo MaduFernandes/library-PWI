157c3fe1e2790a245b77ffdd272b18ce27fc0bee
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2021-06-09 00:03:38 -0300

    style: add styles in forms

--- frontend/src/pages/Forms/BookForms.js
+++ frontend/src/pages/Forms/BookForms.js
@@ -1,12 +1,62 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-import {} from "@material-ui/core";
+import { useStyles } from "./style";
+
+import api from "../../services/api";
 
 const BookForms = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleSave = async (values) => {
+    const response = await api.post("/books", values);
+    console.log(response);
+  };
+
+  const handleCancel = () => {
+    history.push("/");
+  };
   return (
     <>
-      TODO: Criar o formulário
-      <h1>Eaio</h1>
+      <form onSubmit={handleSave} className={classes.form}>
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Nome do Livro"
+          name="name"
+        />
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Autor"
+          name="author"
+        />
+
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Descrição"
+          name="description"
+        />
+        <input
+          className={classes.input}
+          type="text"
+          name="category"
+          placeholder="Categoria"
+        />
+
+        <input
+          className={classes.input}
+          name="price"
+          type="text"
+          placeholder="Preço"
+        />
+      </form>
+      <button className={classes.btnCancel} onClick={handleCancel}>
+        Cancelar
+      </button>
+      <button className={classes.btn}>Salvar</button>
     </>
   );
 };
