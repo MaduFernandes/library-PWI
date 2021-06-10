e8dd8010aadd7baf089c8c6790e52d181d025d89
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2021-06-10 11:38:11 -0300

    feat: add forms

--- frontend/src/pages/Forms/BookForms.js
+++ frontend/src/pages/Forms/BookForms.js
@@ -2,61 +2,129 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 import { useStyles } from "./style";
+import "../../styles/global.css";
 
 import api from "../../services/api";
+
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import CurrencyInput from "react-currency-masked-input";
 
 const BookForms = () => {
   const classes = useStyles();
   const history = useHistory();
 
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    author: Yup.string().required("Author is required"),
+    description: Yup.string().required("Description is required"),
+    category: Yup.string().required("Category is required"),
+    price: Yup.number().required("Price is required"),
+  });
+
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: {
+      name: "",
+      author: "",
+      description: "",
+      category: "",
+      price: "",
+    },
+    validationSchema,
+    onSubmit(values) {
+      handleSave(values);
+    },
+  });
+
   const handleSave = async (values) => {
     const response = await api.post("/books", values);
-    console.log(response);
+
+    if (response.status === 200) {
+      history.push("/");
+    }
   };
 
   const handleCancel = () => {
     history.push("/");
   };
+
   return (
     <>
-      <form onSubmit={handleSave} className={classes.form}>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <input
           className={classes.input}
           type="text"
           placeholder="Nome do Livro"
           name="name"
+          defaultValue={values.name}
+          onChange={handleChange}
         />
+        {errors.name && (
+          <div id="feedback" className={classes.errors}>
+            {errors.name}
+          </div>
+        )}
         <input
           className={classes.input}
           type="text"
           placeholder="Autor"
           name="author"
+          defaultValue={values.author}
+          onChange={handleChange}
         />
-
+        {errors.author && (
+          <div id="feedback" className={classes.errors}>
+            {errors.author}
+          </div>
+        )}
         <input
           className={classes.input}
           type="text"
           placeholder="Descrição"
           name="description"
+          defaultValue={values.description}
+          onChange={handleChange}
         />
+        {errors.description && (
+          <div id="feedback" className={classes.errors}>
+            {errors.description}
+          </div>
+        )}
         <input
           className={classes.input}
           type="text"
           name="category"
           placeholder="Categoria"
+          defaultValue={values.category}
+          onChange={handleChange}
         />
-
-        <input
+        {errors.category && (
+          <div id="feedback" className={classes.errors}>
+            {errors.category}
+          </div>
+        )}
+        <CurrencyInput
           className={classes.input}
           name="price"
-          type="text"
           placeholder="Preço"
+          defaultValue={values.price}
+          onChange={handleChange}
+          required
         />
+        {errors.price && (
+          <div id="feedback" className={classes.errors}>
+            {errors.price}
+          </div>
+        )}
+        <div>
+          <button className={classes.btnCancel} onClick={handleCancel}>
+            Voltar
+          </button>
+          <button className={classes.btn} type="submit">
+            Cadastrar
+          </button>
+        </div>
       </form>
-      <button className={classes.btnCancel} onClick={handleCancel}>
-        Cancelar
-      </button>
-      <button className={classes.btn}>Salvar</button>
     </>
   );
 };
--- frontend/src/pages/Forms/style.js
+++ frontend/src/pages/Forms/style.js
@@ -3,48 +3,24 @@ import { makeStyles, createStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles((theme) =>
   createStyles({
     btn: {
-      width: "23%",
-      height: "60px",
       background: "#673ab7",
-      border: "0",
-      borderRadius: "8px",
-      color: "#fff",
-      fontWeight: "700",
-      marginTop: "16px",
-      display: "inline-block",
-      textAlign: "center",
-      textDecoration: "none",
-      fontSize: "18px",
-      lineHeight: "60px",
-      transition: "filter 0.2s",
-      margin: "20px",
     },
     btnCancel: {
-      width: "23%",
-      height: "60px",
       background: "#616161",
-      border: "0",
-      borderRadius: "8px",
-      color: "#fff",
-      fontWeight: "700",
-      marginTop: "16px",
-      display: "inline-block",
-      textAlign: "center",
-      textDecoration: "none",
-      fontSize: "18px",
-      lineHeight: "60px",
-      transition: "filter 0.2s",
-      margin: "20px",
     },
 
     input: {
       width: "50%",
-      height: "60px",
+      height: "55px",
       color: "#333",
       border: "1px solid #dcdce6",
       borderRadius: "8px",
       padding: "0 24px",
       margin: "20px",
+    },
+    errors: {
+      color: "red",
+      marginRight: "38%",
     },
   })
 );
