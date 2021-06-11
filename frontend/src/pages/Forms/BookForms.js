8751b2d308e7e4a2986725c84e165dd1df71236e
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2021-06-11 17:02:54 -0300

    feat: add title

--- frontend/src/pages/Forms/BookForms.js
+++ frontend/src/pages/Forms/BookForms.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import { TextField, Typography, Paper, IconButton } from "@material-ui/core";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+
 import { useStyles } from "./style";
 
 import api from "../../services/api";
 
 import { useFormik } from "formik";
 
-import CurrencyInput from "react-currency-masked-input";
+import CurrencyTextField from "@unicef/material-ui-currency-textfield";
 
 const BookForms = () => {
   const classes = useStyles();
@@ -40,60 +43,82 @@ const BookForms = () => {
 
   return (
     <>
-      <form className={classes.form} onSubmit={handleSubmit}>
-        <input
-          className={classes.input}
-          type="text"
-          placeholder="Nome do Livro"
-          name="name"
-          defaultValue={values.name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          className={classes.input}
-          type="text"
-          placeholder="Autor"
-          name="author"
-          defaultValue={values.author}
-          onChange={handleChange}
-          required
-        />
-        <input
-          className={classes.input}
-          type="text"
-          placeholder="Descrição"
-          name="description"
-          defaultValue={values.description}
-          onChange={handleChange}
-          required
-        />
-        <input
-          className={classes.input}
-          type="text"
-          name="category"
-          placeholder="Categoria"
-          defaultValue={values.category}
-          onChange={handleChange}
-          required
-        />
-        <CurrencyInput
-          className={classes.input}
-          name="price"
-          placeholder="Preço"
-          defaultValue={values.price}
-          onChange={handleChange}
-          required
-        />
-        <div>
-          <button className={classes.btnCancel} onClick={handleCancel}>
-            Voltar
-          </button>
+      <div className={classes.root}>
+        <div className={classes.div}>
+          <IconButton onClick={handleCancel} className={classes.icon}>
+            <ArrowBackIosIcon />
+          </IconButton>
+          <Typography variant="h2" gutterBottom className={classes.typography}>
+            Livros
+          </Typography>
+        </div>
+        <div className={classes.divButton}>
           <button className={classes.btn} type="submit">
             Cadastrar
           </button>
         </div>
-      </form>
+      </div>
+      <Paper>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <TextField
+            id="outlined-basic"
+            label="Nome do Livro"
+            variant="outlined"
+            className={classes.input}
+            type="text"
+            name="name"
+            defaultValue={values.name}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            id="outlined-basic"
+            label="Autor"
+            variant="outlined"
+            className={classes.input}
+            type="text"
+            name="author"
+            defaultValue={values.author}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            id="outlined-basic"
+            label="Descrição"
+            variant="outlined"
+            className={classes.input}
+            type="text"
+            name="description"
+            defaultValue={values.description}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            id="outlined-basic"
+            label="Categoria"
+            variant="outlined"
+            className={classes.input}
+            type="text"
+            name="category"
+            defaultValue={values.category}
+            onChange={handleChange}
+            required
+          />
+          <CurrencyTextField
+            label="Preço"
+            variant="outlined"
+            name="price"
+            defaultValue={values.price}
+            className={classes.input}
+            currencySymbol="$"
+            outputFormat="number"
+            decimalCharacter="."
+            digitGroupSeparator=","
+            onChange={handleChange}
+            required
+          />
+        </form>
+      </Paper>
     </>
   );
 };
--- frontend/src/pages/Forms/style.js
+++ frontend/src/pages/Forms/style.js
@@ -3,20 +3,41 @@ import { makeStyles, createStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles((theme) =>
   createStyles({
     btn: {
-      background: "#673ab7",
+      background: "#8259cb",
     },
     btnCancel: {
       background: "#616161",
     },
-
     input: {
       width: "50%",
       height: "55px",
-      color: "#333",
-      border: "1px solid #dcdce6",
+      backgroundColor: "#fff",
       borderRadius: "8px",
-      padding: "0 24px",
       margin: "20px",
+    },
+    root: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    typography: {
+      alignItems: "left",
+      color: "#696969",
+    },
+    icon: {
+      marginBottom: "15px",
+    },
+    div: {
+      display: " flex",
+      direction: "row",
+      justifyContent: "flex-start",
+      alignItems: "center",
+    },
+    divButton: {
+      minWidth: "85%",
+      display: "flex",
+      direction: "row",
+      justifyContent: "flex-end",
     },
   })
 );
