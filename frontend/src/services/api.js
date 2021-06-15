c4400d791e4e6aeb0584cab88f9005e149344755
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2021-06-15 11:13:39 -0300

    feat: add headers in configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, HEAD, OPTIONS",
+  },
 });
 
 export default api;
